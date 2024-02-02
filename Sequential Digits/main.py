3cc088606585702c6e23a5402ede23afb371c714
Priyasha Ghosh <90889014+Priyasha2002@users.noreply.github.com>
2024-02-02 23:30:01 +0530

    Day 1

---
+++ Sequential Digits/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        digits = "123456789"
+        result = []
+        nl= len(str(low))
+        nh= len(str(high))
+
+        for i in range (nl , nh+1):
+            for j in range (0 , 10-i):
+                num = int(digits [j : j+i])
+                if num>= low and num<= high:
+                    result.append(num)
+        return result
