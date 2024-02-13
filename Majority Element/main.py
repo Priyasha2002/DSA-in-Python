b76ef9aab49317f3187c72263f2c720a64cf0383
Priyasha Ghosh <90889014+Priyasha2002@users.noreply.github.com>
2024-02-13 09:03:14 +0530

    Day 11

--- Majority Element/main.py
+++ Majority Element/main.py
@@ -8,3 +8,17 @@ class Solution:
          count += (1 if n == res else -1)
         return res 
         
+#Another Solution 
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        from collections import Counter
+        counter = Counter(nums)
+        output = 0
+        max_count = 0
+
+        for k , v in counter.items():
+            if v > max_count :
+                max_count += v
+                output = k
+        return output 
